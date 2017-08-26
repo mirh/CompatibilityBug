242c8d32328ced53cea8b3746896233c7b506e51
mirh <mirh@users.noreply.github.com>
2017-08-26 17:16:34 +0200

    Commit

---
+++ MassEffect/MassEffect.cpp
@@ -0,0 +1,25 @@
+#include "stdafx.h"
+#include "Windows.h"
+
+
+BOOL IsElevated() {
+	BOOL fRet = FALSE;
+	HANDLE hToken = NULL;
+	if (OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &hToken)) {
+		TOKEN_ELEVATION Elevation;
+		DWORD cbSize = sizeof(TOKEN_ELEVATION);
+		if (GetTokenInformation(hToken, TokenElevation, &Elevation, sizeof(Elevation), &cbSize)) {
+			fRet = Elevation.TokenIsElevated;
+		}
+	}
+	if (hToken) {
+		CloseHandle(hToken);
+	}
+	return !fRet;
+}
+
+int main()
+{
+	volatile float boom = 1 / IsElevated();
+	return 0;
+}
